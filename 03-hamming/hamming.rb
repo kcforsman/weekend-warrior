54ba060e61a9c9de10ee2fa03bb7f27b5073b535
Kaitlin Forsman <kcforsman@outlook.com>
2018-03-23 15:57:30 -0700

    finish hamming method with all tests passing

---
+++ 03-hamming/hamming.rb
@@ -0,0 +1,16 @@
+class Hamming
+
+  def self.compute strand_1, strand_2
+    return 0 if strand_1 == strand_2
+    length_1 = strand_1.length
+    raise ArgumentError.new("strands not even in length") if length_1 != strand_2.length
+    distance = 0
+    length_1.times do |x|
+      if strand_1[x] != strand_2[x]
+        distance += 1
+      end
+    end
+    distance
+  end
+
+end
